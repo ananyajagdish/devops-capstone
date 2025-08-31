77050c99cc6453b391ff6aa11738280fb9a53129
ananyajagdish <159645633+ananyajagdish@users.noreply.github.com>
2025-08-30 20:21:14 -0400

    Create test_app.py

---
+++ app/tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+
+from devopscapstone.app.src.app import get_dow
+
+def test_get_dow():
+    assert get_dow('2025-08-28') == 'The day of the week is Thursday'
+    assert get_dow('2025-08-31') == 'The day of the week is Sunday'
+
+def test_get_dow_invalid():
+    assert get_dow('2025-08-32') == 'Invalid Date'
